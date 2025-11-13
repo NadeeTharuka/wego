11ee0465ee067273e7760277a81610b933af7227
Nadee Tharuka <121155318+NadeeTharuka@users.noreply.github.com>
2025-11-13 18:42:26 +0530

    changes

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,8 @@
-import { render } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders app without crashing', () => {
-  render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  );
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
