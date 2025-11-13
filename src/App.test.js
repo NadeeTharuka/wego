79a3f4a7eff3fc9dbcd7792602add0b00b63d9dd
Nadee Tharuka <121155318+NadeeTharuka@users.noreply.github.com>
2025-11-13 18:36:00 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
-// Delete or comment out the failing test
 import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders app', () => {
+test('renders app without crashing', () => {
   render(
     <BrowserRouter>
       <App />
