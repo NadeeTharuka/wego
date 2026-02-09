4e308a594473405706f9daa0dcff874d6b084776
Nadee Tharuka <121155318+NadeeTharuka@users.noreply.github.com>
2026-02-09 18:29:24 +0530

    Update Tours.jsx

--- src/pages/Tours.jsx
+++ src/pages/Tours.jsx
@@ -453,7 +453,7 @@ const TourPackages = () => {
       {/* Hero Header */}
 <div className="container-fluid bg-primary py-5 mb-5 hero-header"
   style={{
-  backgroundImage: 'linear-gradient(rgba(20, 20, 31, 0.7), rgba(20, 20, 31, 0.7)), url("https://images.unsplash.com/photo-1546708973-b339540b5162?ixlib=rb-4.0.3&auto=format&fit=crop&w=1200")',    backgroundPosition: 'center center',
+  backgroundImage: 'linear-gradient(rgba(20, 20, 31, 0.5), rgba(20, 20, 31, 0.7)), url("https://images.unsplash.com/photo-1546708973-b339540b5162?ixlib=rb-4.0.3&auto=format&fit=crop&w=1200")',    backgroundPosition: 'center center',
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'cover',
     minHeight: '95vh',
