ad646970cd4ff0e1cda8c770e1c56d1da1e65262
Nadee Tharuka <121155318+NadeeTharuka@users.noreply.github.com>
2026-02-20 20:47:03 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -139,7 +139,7 @@ export default function Home() {
         <div style={{
           position: 'absolute',
           top: 0, left: 0, right: 0, bottom: 0,
-          backgroundColor: 'rgba(0, 0, 0, 0.15)',
+          backgroundColor: 'rgba(0, 0, 0, 0.01)',
           zIndex: 0
         }} />
         <div className="container py-5" style={{ position: 'relative', zIndex: 1 }}>
